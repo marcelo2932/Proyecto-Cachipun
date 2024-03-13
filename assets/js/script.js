e2ada5daac84878020436c0a9af7dfcb79473200
marcelo2932 <marcelohenriquezrenca@gmail.com>
2024-03-12 22:30:55 -0300

    Se agrega opcion visual promt de la maquina

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,28 @@ let jugadas = ['piedra', 'papel', 'tijera']
 let puntajeUser=0;
 let puntajeMaquina =0;
 
+
 let numeroJuagadas = parseInt(prompt('Cuantas veces deseas jugar en este torneo?'))
+
+/* Solicitar al user su jugada, esta operacion se realizara en cada juego {piedra, papel, tijera}*/
 
 for(let i = 0; i < numeroJuagadas; i++) {
     let jugadaUser = prompt('Ingrese la jugada que deseas jugar (piedra, papel, tijera): ').toLowerCase()
     if(!jugadas.includes(jugadaUser)) {
         alert('opcion no valida')
     }
-    
+
+/* Aplicar math.random para la seleccion de la jugada de la maquina */
+
     let indiceMaquina = Math.floor(Math.random() * jugadas.length)
     
     let opcionMaquina = jugadas[indiceMaquina]
+
+    if(opcionMaquina) {
+        alert(`La maquina elige ${opcionMaquina}`)
+    }
+
+/* Evaluar la seleccion del user vs la maquina  */
 
     if(jugadaUser === opcionMaquina) {
         alert(`Empate - La maquina elige ${opcionMaquina} y tu elegiste ${jugadaUser}`)
@@ -30,6 +41,8 @@ for(let i = 0; i < numeroJuagadas; i++) {
     }
 }
 
+/* Indicar el resultado de la partida dependiendo del caso */
+
 if(puntajeUser === puntajeMaquina) {
     alert('Empate')
 }else if(puntajeUser > puntajeMaquina){
@@ -37,7 +50,6 @@ if(puntajeUser === puntajeMaquina) {
 }else {
     alert(`Perdiste el Torneo puntajes: Jugador ${puntajeUser} - Maquina ${puntajeMaquina}`)
 }
-/* Solicitar al user su jugada, esta operacion se realizara en cada juego {piedra, papel, tijera}*/
-/* Aplicar math.random para la seleccion de la jugada de la maquina */
-/* Evaluar la seleccion del user vs la maquina  */
-/* Indicar el resultado de la partida dependiendo del caso */
+
+
+
